eea9b75cf93e92421e8e8e2f53c0843a64bb4be3
correa1victor <victorcorreiadasilva@gmail.com>
2020-12-30 22:47:24 -0300

    novo
    novo

---
+++ trabalho/javascript/jquery.js
@@ -0,0 +1,20 @@
+		
+		
+		$(document).ready(function(){
+
+			$(".formulario").fadeOut("slow");
+			$(".formulario").fadeIn("slow");
+
+		});
+
+
+		$(document).ready(function(){
+
+			$(".formulario2").fadeOut("slow");		
+			$(".formulario2").fadeIn("slow");
+			
+		});
+
+		
+
+		
