33414c728c0bd1e2f62d299a67bbc045accd1d7b
correa1victor <victorcorreiadasilva@gmail.com>
2021-01-05 00:00:21 -0300

    new 05-01-20

--- trabalho/javascript/script.js
+++ trabalho/javascript/script.js
@@ -1,3 +1,28 @@
 
+	function enviar()
+	{
 
-		
+	    var nomeValue = document.querySelector(".js-input-name").value;
+	    var emailValue = document.querySelector(".js-input-email").value;
+	    var moradaValue = document.querySelector(".js-input-morada").value;
+	    var idadeValue = document.querySelector(".js-input-idade").value;
+	    var sexoValue = document.querySelector(".js-input-sexo").value;
+	    var msgValue = document.querySelector(".js-input-msg").value;
+
+
+	    var formValue = 
+	    {       
+
+	        name: nomeValue,
+	        email: emailValue,
+	        data: 
+	        {                                     
+	            morada: moradaValue, 
+	            idade: idadeValue,
+	            sexo: sexoValue,
+	            msg: msgValue    
+	        }
+	    };
+
+	    console.log(formValue);
+}
